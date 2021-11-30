91b7e68b01e562969326524ee1f63f8ec7fda6f1
John Parkhurst <jparkhurst120@gmail.com>
2021-11-30 15:27:05 -0500

    Refactoring

--- Script/build.sh
+++ Script/build.sh
@@ -1,9 +1,11 @@
 #John Parkhurst
-#Not exactly sure why combined.c is there
+#This script will build all versions we desire!
+
 #gcc "./Data-Sets/Data Production/combined.c" -o prog
 #./prog -r ./Data-Sets/Data-Set-1--2-procs/img.tga ./Output
 echo "Begin Young Padiwan"
 rm prog
-gcc "./Source/prog05.c" -o prog
-./prog ./Data-Sets/Data-Set-1--2-procs/ ./Output
+gcc "./Source/prog05_V1.c" -o prog
+#Data set 1 and 2 processes
+./prog 2 ./Data-Sets/Data-Set-1--2-procs/ ./Output
 echo "Done!"
