412ebe34cc545e06d83021559e56057fbeae1bf2
John Parkhurst <jparkhurst120@gmail.com>
2021-11-30 14:36:40 -0500

    Starting the program
    Quickly setup some basic functions and such mainly to open directories, open files, and added a previous code sample  of mine (System commands) using pipes to help base the functionality of it off.

--- Script/build.sh
+++ Script/build.sh
@@ -3,7 +3,7 @@
 #gcc "./Data-Sets/Data Production/combined.c" -o prog
 #./prog -r ./Data-Sets/Data-Set-1--2-procs/img.tga ./Output
 echo "Begin Young Padiwan"
-
-
-
-echo "Done! a"
+rm prog
+gcc "./Source/prog05.c" -o prog
+./prog ./Data-Sets/Data-Set-1--2-procs/ ./Output
+echo "Done!"
---
+++ Source/prog05.c
@@ -0,0 +1,120 @@
+/**
+ * John T Parkhurst
+ * 11/30/21
+ * Pipes and Threads
+ * */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+//THIS IS DEAD CODE
+void extraCredit(char *comm){
+    /* param: nothing
+     *  This is for Extra credit 2.8
+     *  did not get to fully implement it
+     *  Check report for synopsis of this
+     * return: nothing
+     */
+    int pid = fork();
+    if (pid == 0){
+        //Argument list for exevp
+        //Fill in Comm to argument list with
+        //arglist[i]= strtok(comm,"")
+        char *argument_list[] = {"ls", "-la", NULL};
+        //We set filename = to du_1.out,du_2.out as needed
+        //Here is eclog.out for testing purposes
+
+        const char *filename = "eclog.out";
+        //Make sure filename not null
+        if (filename){
+            //opens file for writing only
+            int fd = open(filename, O_WRONLY, 0666);
+            //WE open up Pipe with dup2
+            //Redirecting all output//errors
+            //To the File fd
+            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDERR_FILENO);
+            //Right here we exec
+            execvp(argument_list[0], argument_list);
+            close(fd);
+            printf("ONLY IF ERROR\n");
+            printf("CHECK EXEC PARAMATERS\n");
+            exit(-23);
+        }
+        else{
+            printf("ERROR OPENING LOG FILE\n");
+            exit(-23);
+        }
+    }
+    else{
+        waitpid(pid, &pid, 1);
+        printf("Extra Credit is Done\n");
+    }
+}
+
+
+FILE *openF(char* arr){
+    //@param: pointer to char array
+    //Opens the file
+    //return: none
+    FILE *fptr;
+    //REMEBER TO USE DOULBE QUOTES
+    fptr = fopen(arr,"r");
+    if(fptr ==NULL){
+        //Use put to not include null character
+        puts("Error While Opening the File");
+        fclose(fptr);
+        exit(69);
+    }else{
+        return fptr;
+    }
+}
+
+DIR *openD(const char* arr){
+    /*
+    @Param: Charachter array(string) of the input folder
+    return: pointer to a directory
+    */
+    struct dirent *dp;
+    DIR *dir;
+    const char *dirStr;
+    dirStr = arr;
+    //Checks if we can open the directory properaly
+    dir = opendir(dirStr);
+    if ((dir) == NULL){
+        //Print Error then Exit application
+        fprintf(stderr, "Can't open %s\n", dirStr);
+        exit(3);
+    }
+    return dir;
+}
+void process(DIR* direc){
+    /*
+    @param:
+    */
+   return;
+}
+void output(char* filNam){
+
+    return;
+}
+
+int main(int argc, char const *argv[])
+{
+    //Assigning the directory object
+    DIR *dir;
+    dir = openD(argv[1]);
+
+    
+
+
+
+
+    closedir(dir);
+
+
+    return 0;
+}
